062ddab19ac479a98f694856495455f562c06a8b
Marcelo Fuchs <marcelof29@gmail.com>
2018-09-18 10:32:43 -0300

    ajustes

--- config/dependencies.php
+++ config/dependencies.php
@@ -1,5 +1,6 @@
 <?php
-require_once 'dependencies.pdo.php';
+require_once 'dependencies.doctrine.php';
+//require_once 'dependencies.pdo.php';
 //require_once 'dependencies.redbean.php';
 
 // DATABASE DEFAULT PRELOAD
